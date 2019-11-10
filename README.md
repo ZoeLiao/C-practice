6a0540b29f57a69e03f0d1385bfc79d3bb1a4892
ZoeLiao <zoejoyuliao@gmail.com>
2019-11-10 14:44:01 +0800

    Add base.c

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # C-practice
+
+## How to run c script:
+- `cc -o <filename> <program_name.c>`
+- `.<filename>`
---
+++ src/base.c
@@ -0,0 +1,13 @@
+/* include header file */
+#include <stdio.h>
+
+/* return int and do not require any parameter */
+int main(void){
+    int num;
+    num = 2;
+    printf("I have %d cats.\n", num);
+    if(num < 5){
+        printf("the num is smaller than 5");
+    }
+    return 0;
+}
