4da47640a986a3bdcc488d6a413a237723ad1e6e
ZoeLiao <zoejoyuliao@gmail.com>
2020-01-03 23:58:23 +0800

    Add matric in array.c and call by address

--- src/array.c
+++ src/array.c
@@ -3,14 +3,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+#define SIZE 4
+#define ROW 2
+#define COL 3
+
+void sum_matrix(int arr_a[][COL], int arr_b[][COL]);
+
 int main(void) {
 
-    int formal[4] = {87, 88, 92, 94};
-    float same_variables[4] = {0.0};
+    int formal[SIZE] = {87, 88, 92, 94};
+    float same_variables[SIZE] = {0.0};
     double without_size[] = {1, 2, 3, 4}; 
-    printf("size of int array (4 variables): %lu\n", sizeof(formal));
-    printf("size of float array (4 variables): %lu\n", sizeof(same_variables));
-    printf("size of double array (4 variables): %lu\n", sizeof(without_size));
+    printf("size of int array (%d variables): %lu\n", SIZE, sizeof(formal));
+    printf("size of float array (%d variables): %lu\n", SIZE, sizeof(same_variables));
+    printf("size of double array (%d variables): %lu\n", SIZE, sizeof(without_size));
 
     char string[3];
     int i;
@@ -23,5 +30,23 @@ int main(void) {
         printf("string[%d] = %c\n", i, string[i]);
     }
 
+    // Two-dimensional array
+    int sale[][3] = {{1, 2, 3},
+                      {4, 5, 6}};
+
+    int matrix_a[ROW][COL] = {{1, 1, 1}, {3, 3, 3}};
+    int matrix_b[ROW][COL] = {{1, 1, 5}, {4, 4, 4}};
+
+    // call by address instead of values
+    sum_matrix(matrix_a, matrix_b);
     return 0;
+}
+
+void sum_matrix(int arr_a[][COL], int arr_b[][COL]){
+    int i, j;
+    for(i=0; i<ROW; i++){
+        for(j=0; j<COL; j++){
+            printf("sum = %d\n", arr_a[i][j] + arr_b[i][j]);
+        }
+    }
 }
